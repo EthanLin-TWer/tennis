0ec1d0a7a8460394934ab5986b30e0f904aa939c
Linesh <linesh.simpcity@gmail.com>
2019-10-27 01:09:10 +0800

    ♻️refactor: simplify calculateNormalScores()

--- src/game1.js
+++ src/game1.js
@@ -24,22 +24,16 @@ export class TennisGame1 {
   }
 
   calculateNormalScores() {
-    let score = ''
     switch (this.m_score1) {
       case 0:
-        score = 'Love-All'
-        break
+        return 'Love-All'
       case 1:
-        score = 'Fifteen-All'
-        break
+        return 'Fifteen-All'
       case 2:
-        score = 'Thirty-All'
-        break
+        return 'Thirty-All'
       default:
-        score = 'Deuce'
-        break
+        return 'Deuce'
     }
-    return score
   }
 
   calculateFinalScores() {
