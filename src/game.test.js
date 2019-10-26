cefbefde8b91d874dd51bea27413a8c8928c9e3a
Linesh <linesh.simpcity@gmail.com>
2019-10-27 00:56:06 +0800

    ✨feature: initial version of the game

---
+++ src/game.test.js
@@ -0,0 +1,133 @@
+import { TennisGame1 } from './game1'
+import { TennisGame2 } from './game2'
+import { TennisGame3 } from './game3'
+
+describe('TennisGame', () => {
+  const allScores = [
+    [0, 0, 'Love-All'],
+    [1, 1, 'Fifteen-All'],
+    [2, 2, 'Thirty-All'],
+    [3, 3, 'Deuce'],
+    [4, 4, 'Deuce'],
+
+    [1, 0, 'Fifteen-Love'],
+    [0, 1, 'Love-Fifteen'],
+    [2, 0, 'Thirty-Love'],
+    [0, 2, 'Love-Thirty'],
+    [3, 0, 'Forty-Love'],
+    [0, 3, 'Love-Forty'],
+    [4, 0, 'Win for player1'],
+    [0, 4, 'Win for player2'],
+
+    [2, 1, 'Thirty-Fifteen'],
+    [1, 2, 'Fifteen-Thirty'],
+    [3, 1, 'Forty-Fifteen'],
+    [1, 3, 'Fifteen-Forty'],
+    [4, 1, 'Win for player1'],
+    [1, 4, 'Win for player2'],
+
+    [3, 2, 'Forty-Thirty'],
+    [2, 3, 'Thirty-Forty'],
+    [4, 2, 'Win for player1'],
+    [2, 4, 'Win for player2'],
+
+    [4, 3, 'Advantage player1'],
+    [3, 4, 'Advantage player2'],
+    [5, 4, 'Advantage player1'],
+    [4, 5, 'Advantage player2'],
+    [15, 14, 'Advantage player1'],
+    [14, 15, 'Advantage player2'],
+
+    [6, 4, 'Win for player1'],
+    [4, 6, 'Win for player2'],
+    [16, 14, 'Win for player1'],
+    [14, 16, 'Win for player2'],
+  ]
+
+  const checkScore = function(
+    reporter,
+    TennisGame,
+    player1Score,
+    player2Score,
+    expectedScore
+  ) {
+    const highestScore = Math.max(player1Score, player2Score)
+    let game
+    let result
+    let message = ''
+    let ok = false
+    let i
+
+    try {
+      game = new TennisGame('player1', 'player2')
+      for (i = 0; i < highestScore; i++) {
+        if (i < player1Score) {
+          game.wonPoint('player1')
+        }
+        if (i < player2Score) {
+          game.wonPoint('player2')
+        }
+      }
+      result = game.getScore()
+
+      if (result === expectedScore) {
+        ok = true
+      } else {
+        message = `Result = '${result}'`
+      }
+    } catch (ex) {
+      message = `Exception: ${ex}`
+    }
+
+    reporter.addCase(expectedScore, ok, message)
+  }
+
+  const runSuiteOnGame = function(reporter, TennisGame, title) {
+    reporter.addSuite(title)
+    allScores.forEach(function(score) {
+      checkScore(reporter, TennisGame, score[0], score[1], score[2])
+    })
+  }
+
+  const getConsoleReporter = function() {
+    const reporter = {
+      errors: 0,
+      addSuite(title) {
+        console.log(`Running suite '${title}'...`)
+      },
+      addCase(title, ok, message) {
+        if (!ok) {
+          console.log(`Case '${title}': ${message}`)
+          this.errors++
+        }
+      },
+      done() {
+        if (this.errors > 0) {
+          console.log(`Got ${this.errors} failure(s)!`)
+        } else {
+          console.log('Done, all OK ')
+        }
+      },
+    }
+
+    return reporter
+  }
+
+  const reporter = getConsoleReporter()
+
+  afterEach(() => {
+    reporter.done()
+  })
+
+  it('should return correct result for game 1', () => {
+    runSuiteOnGame(reporter, TennisGame1, 'TennisGame1')
+  })
+
+  it('should return correct result for game 2', () => {
+    runSuiteOnGame(reporter, TennisGame2, 'TennisGame2')
+  })
+
+  it('should return correct result for game 3', () => {
+    runSuiteOnGame(reporter, TennisGame3, 'TennisGame3')
+  })
+})
---
+++ src/game2.js
@@ -0,0 +1,107 @@
+export class TennisGame2 {
+  constructor(player1Name, player2Name) {
+    this.P1point = 0
+    this.P2point = 0
+
+    this.P1res = ''
+    this.P2res = ''
+
+    this.player1Name = player1Name
+    this.player2Name = player2Name
+  }
+
+  getScore() {
+    let score = ''
+
+    if (this.P1point === this.P2point && this.P1point < 3) {
+      if (this.P1point === 0) score = 'Love'
+      if (this.P1point === 1) score = 'Fifteen'
+      if (this.P1point === 2) score = 'Thirty'
+      score += '-All'
+    }
+    if (this.P1point === this.P2point && this.P1point > 2) score = 'Deuce'
+
+    if (this.P1point > 0 && this.P2point === 0) {
+      if (this.P1point === 1) this.P1res = 'Fifteen'
+      if (this.P1point === 2) this.P1res = 'Thirty'
+      if (this.P1point === 3) this.P1res = 'Forty'
+
+      this.P2res = 'Love'
+      score = `${this.P1res}-${this.P2res}`
+    }
+    if (this.P2point > 0 && this.P1point === 0) {
+      if (this.P2point === 1) this.P2res = 'Fifteen'
+      if (this.P2point === 2) this.P2res = 'Thirty'
+      if (this.P2point === 3) this.P2res = 'Forty'
+
+      this.P1res = 'Love'
+      score = `${this.P1res}-${this.P2res}`
+    }
+
+    if (this.P1point > this.P2point && this.P1point < 4) {
+      if (this.P1point === 2) this.P1res = 'Thirty'
+      if (this.P1point === 3) this.P1res = 'Forty'
+      if (this.P2point === 1) this.P2res = 'Fifteen'
+      if (this.P2point === 2) this.P2res = 'Thirty'
+      score = `${this.P1res}-${this.P2res}`
+    }
+    if (this.P2point > this.P1point && this.P2point < 4) {
+      if (this.P2point === 2) this.P2res = 'Thirty'
+      if (this.P2point === 3) this.P2res = 'Forty'
+      if (this.P1point === 1) this.P1res = 'Fifteen'
+      if (this.P1point === 2) this.P1res = 'Thirty'
+      score = `${this.P1res}-${this.P2res}`
+    }
+
+    if (this.P1point > this.P2point && this.P2point >= 3) {
+      score = 'Advantage player1'
+    }
+
+    if (this.P2point > this.P1point && this.P1point >= 3) {
+      score = 'Advantage player2'
+    }
+
+    if (
+      this.P1point >= 4 &&
+      this.P2point >= 0 &&
+      this.P1point - this.P2point >= 2
+    ) {
+      score = 'Win for player1'
+    }
+    if (
+      this.P2point >= 4 &&
+      this.P1point >= 0 &&
+      this.P2point - this.P1point >= 2
+    ) {
+      score = 'Win for player2'
+    }
+    return score
+  }
+
+  SetP1Score(number) {
+    let i
+    for (i = 0; i < number; i++) {
+      this.P1Score()
+    }
+  }
+
+  SetP2Score(number) {
+    let i
+    for (i = 0; i < number; i++) {
+      this.P2Score()
+    }
+  }
+
+  P1Score() {
+    this.P1point++
+  }
+
+  P2Score() {
+    this.P2point++
+  }
+
+  wonPoint(player) {
+    if (player === 'player1') this.P1Score()
+    else this.P2Score()
+  }
+}
